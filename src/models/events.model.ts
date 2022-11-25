5d10c28b63b356f662e6cdc2a7ba1995336264ab
hont-dev_lhy <hywkd3@gmail.com>
2022-11-25 23:26:39 +0900

    추가: event model

---
+++ src/models/events.model.ts
@@ -0,0 +1,43 @@
+import { Event } from '@interfaces/events.interface';
+
+// password: password
+const eventModel: Event[] = [
+  {
+    begin_at: 1620268200,
+    end_at: 1620275400,
+    created_at: 1620272253,
+    updated_at: 1620272253,
+  },
+  {
+    begin_at: 1620275400,
+    end_at: 1620275400,
+    created_at: 1620272253,
+    updated_at: 1620272253,
+  },
+  {
+    begin_at: 1620276300,
+    end_at: 1620275400,
+    created_at: 1620272253,
+    updated_at: 1620272253,
+  },
+  {
+    begin_at: 1620354600,
+    end_at: 1620354900,
+    created_at: 1620272253,
+    updated_at: 1620272253,
+  },
+  {
+    begin_at: 1620441000,
+    end_at: 1620469800,
+    created_at: 1620272253,
+    updated_at: 1620272253,
+  },
+  {
+    begin_at: 1620477000,
+    end_at: 1620534600,
+    created_at: 1620272253,
+    updated_at: 1620272253,
+  },
+];
+
+export default eventModel;
